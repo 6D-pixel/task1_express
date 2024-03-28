cd10eb34cb2b51292ac4cb6850df75dddcf9465e
6D-pixel <dinesh7rd@gmail.com>
2024-03-28 11:41:27 +0530

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const axios = require("axios");
 require("dotenv").config();
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
 const port = process.env.PORT || 3001;
 const apiKey = process.env.API_KEY;
 
